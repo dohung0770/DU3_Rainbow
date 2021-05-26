1c948268015e33911562b7194bdb8cb545e92c49
HungDo <dqhung3@ldap.cmcglobal.com.vn>
2021-05-26 09:50:05 +0700

    Add routing & store Add shared component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,4 @@
-<template>
+s<template>
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h3>Installed CLI Plugins</h3>
@@ -81,15 +81,15 @@
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
-    msg: String,
-  },
-};
+    msg: String
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
 h3 {
   margin: 40px 0 0;
 }
